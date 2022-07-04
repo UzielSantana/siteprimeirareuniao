6146c042b93abe19c1fabe464347da1e913eb21b
UzielSantana <104090820+UzielSantana@users.noreply.github.com>
2022-07-04 16:33:30 -0300

    Primeiros arquivos do projeto

---
+++ script.js
@@ -0,0 +1,210 @@
+function passavalor() {
+  
+    if(document.getElementById("check1").checked) {
+      let nomeinfo = document.getElementById("check1").value;
+      localStorage.setItem("textovalue", nomeinfo);
+      
+      return false
+    }
+    
+  
+    else {
+       localStorage.setItem("textovalue", "não.");
+  
+    }
+  
+  }
+  
+  
+function passavalor2() {
+    
+    if(document.getElementById("check2").checked) {
+      let nomeinfo2 = document.getElementById("check2").value;
+      localStorage.setItem("textovalue2", nomeinfo2);
+      
+      return false
+    }
+    
+  
+    else {
+      localStorage.setItem("textovalue2", "não.");
+  
+    }
+  
+    
+  
+}
+
+function passavalor3() {
+  
+    if(document.getElementById("check3").checked) {
+      let nomeinfo3 = document.getElementById("check3").value;
+      localStorage.setItem("textovalue3", nomeinfo3);
+      
+      return false
+    }
+    
+  
+    else {
+       localStorage.setItem("textovalue3", "não.");
+  
+    }
+  
+}
+  
+  
+
+  function passavalor4() {
+    
+    if(document.getElementById("check4").checked) {
+      let nomeinfo4 = document.getElementById("check4").value;
+      localStorage.setItem("textovalue4", nomeinfo4);
+      
+      return false
+    }
+    
+  
+    else {
+      localStorage.setItem("textovalue4", "não.");
+  
+    }
+  
+    
+  
+  }
+
+function passavalor5() {
+  
+    if(document.getElementById("check5").checked) {
+      let nomeinfo5 = document.getElementById("check5").value;
+      localStorage.setItem("textovalue5", nomeinfo5);
+      
+      return false
+    }
+    
+  
+    else {
+       localStorage.setItem("textovalue5", "não.");
+  
+    }
+  
+  }
+  
+  
+function passavalor6() {
+    
+    if(document.getElementById("check6").checked) {
+      let nomeinfo6 = document.getElementById("check6").value;
+      localStorage.setItem("textovalue6", nomeinfo6);
+      
+      return false
+    }
+    
+  
+    else {
+      localStorage.setItem("textovalue6", "não");
+  
+    }
+  
+    
+  
+  }
+function passavalor7() {
+  
+    if(document.getElementById("check7").checked) {
+      let nomeinfo7 = document.getElementById("check7").value;
+      localStorage.setItem("textovalue7", nomeinfo7);
+      
+      return false
+    }
+    
+  
+    else {
+       localStorage.setItem("textovalue7", "não");
+  
+    }
+  
+  }
+  
+  
+function passavalor8() {
+    
+    if(document.getElementById("check8").checked) {
+      let nomeinfo8 = document.getElementById("check8").value;
+      localStorage.setItem("textovalue8", nomeinfo8);
+      
+      return false
+    }
+    
+  
+    else {
+      localStorage.setItem("textovalue8", "não");
+  
+    }
+  
+    
+  
+  }
+function passavalor9() {
+  
+    if(document.getElementById("check9").checked) {
+      let nomeinfo9 = document.getElementById("check9").value;
+      localStorage.setItem("textovalue9", nomeinfo9);
+      
+      return false
+    }
+    
+  
+    else {
+       localStorage.setItem("textovalue9", "não");
+  
+    }
+  
+  }
+  
+  
+function passavalor10() {
+    
+    if(document.getElementById("check10").checked) {
+      let nomeinfo10 = document.getElementById("check10").value;
+      localStorage.setItem("textovalue10", nomeinfo10);
+      
+      return false
+    }
+    
+  
+    else {
+      localStorage.setItem("textovalue10", "não");
+  
+    }
+  
+    
+  
+  }
+function passavalor11() {
+  
+    if(document.getElementById("check11").checked) {
+      let nomeinfo11 = document.getElementById("check11").value;
+      localStorage.setItem("textovalue11", nomeinfo11);
+      
+      return false
+    }
+    
+  
+    else {
+       localStorage.setItem("textovalue", "não");
+  
+    }
+  
+  }
+  
+  
+function passatexto() {
+  
+      let comentario = document.getElementById("comentarios").value;
+      localStorage.setItem("textovalue12", comentario);
+      
+      return false
+    }
+    
+  
